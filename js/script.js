b65d6632ff588f3906f424cbc99a2c3a299a6d0a
anhutf <anhutf@gmail.com>
2022-11-07 22:02:35 +0700

    Update code 2

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const projectList = document.querySelectorAll(".project");
+
+// Get data
+const getProject = (index) => {
+  const detailView = document.querySelector(".detail-view");
+  const languages = document.querySelector(".languages");
+  const demo = document.querySelector(".demo");
+  const sourceCode = document.querySelector(".source-code");
+
+  fetch(`data/project-list.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      detailView.src = data[index]["demo"];
+      detailView.title = data[index]["title"];
+
+      languages.innerText = data[index]["languages"];
+
+      demo.innerText = data[index]["demo"];
+      demo.href = data[index]["demo"];
+
+      sourceCode.innerText = data[index]["source-code"];
+      sourceCode.href = data[index]["source-code"];
+    });
+};
+
+projectList.forEach((project, index) => {
+  project.addEventListener("click", () => {
+    getProject(index);
+  });
+});
